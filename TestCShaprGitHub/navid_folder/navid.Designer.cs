2cea19bb4ac6162bb28585da3d9f03a89617795d
Vid3107 <navidmeng3107@gmail.com>
2023-10-24 09:46:26 +0700

    add folder navid2

--- TestCShaprGitHub/navid_folder/navid.Designer.cs
+++ TestCShaprGitHub/navid_folder/navid.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(281, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(202, 95);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "navid";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // navid
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "navid";
             this.Text = "navid";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
---
+++ TestCShaprGitHub/navid_folder2/navid2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCShaprGitHub.navid_folder2
+{
+    internal class navid2
+    {
+    }
+}
